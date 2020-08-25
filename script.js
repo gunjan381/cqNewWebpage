5d69ec2812ac77eff74e36ba4db7126731144f22
gunjan381 <56755316+gunjan381@users.noreply.github.com>
2020-08-25 21:39:25 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    $('.flowfromright').css({
+        //width: (50 + scroll / 25) + "%"
+        left: scroll / 100 + "%"
+    })
+    $('.flowfromleft').css({
+        //width: (50 + scroll / 5) + "%",
+        right: scroll / 100 + "%"
+    })
+
+});
